bf3daa33de11e129f7b2f4f87376ee96ad417d2f
SowkatAlam <sowkat.alam5@gmail.com>
2015-09-28 16:36:13 +0600

    First commit

---
+++ CsharpConsoleApplication/CsharpConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is first commit code.\n");
+        }
+    }
+}
